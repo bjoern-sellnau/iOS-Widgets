3034e4f4d68f55dae808a2d8c17d90fd05237a43
Bjoern Sellnau <bjoern-sellnau@users.noreply.github.com>
2021-07-24 21:18:06 +0200

    fix

--- number-of-covild-19-vaccinations/number-of-covild-19-vaccinations.js
+++ number-of-covild-19-vaccinations/number-of-covild-19-vaccinations.js
@@ -326,7 +326,6 @@ if (config.widgetFamily === 'large') {
     const progressBar = row.addImage(creatProgress(value.fullyVaccinated.quote))
     progressBar.imageSize = new Size(width, h)
     
-    row.addSpacer()
     const vaccinatedText = row.addText(`${parseInt(value.fullyVaccinated.quote).toLocaleString(Device.language())}`)
     vaccinatedText.font = Font.systemFont(fontSize)
   }
@@ -346,7 +345,6 @@ if (config.widgetFamily === 'large') {
   const progressBar = row.addImage(creatProgress(germany.fullyVaccinated.quote))
   progressBar.imageSize = new Size(width, h)
   
-   row.addSpacer()
     const vaccinatedText = row.addText(`${parseInt(germany.fullyVaccinated.quote).toLocaleString(Device.language())}`)
     vaccinatedText.font = Font.systemFont(fontSize)
   
